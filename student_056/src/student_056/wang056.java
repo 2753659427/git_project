a09abbf7321e58f10c229db822393435ed99a7b4
2753659427 <2753659427@qq.com>
2020-06-18 17:14:54 +0800

    wangjingwen

---
+++ student_056/src/student_056/wang056.java
@@ -0,0 +1,18 @@
+package student_056;
+
+public class wang056 {
+	public int id;
+	public String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
